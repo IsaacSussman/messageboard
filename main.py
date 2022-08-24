6764d091856659044ad7c8fa0b8224a4402b3903
IsaacSussman <111471257+IsaacSussman@users.noreply.github.com>
2022-08-24 18:14:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ast import Str
 import datetime as dt
 from pydoc import doc
-import tkinter
+import tkinter as tk
 
 def getTimeStyles(timeInput: dt.datetime,isInternationalTime:bool):
     pass
@@ -11,32 +11,32 @@ def datetimeToString(timeInput: dt.datetime, isInternationalTime:bool) -> Str:
     return timeInput.strftime("%m-%d-%y at %H:%M:%S ")
     #return str(timeInput.strftime("%m")+"-"+timeInput.strftime("%d")+"-"+timeInput.strftime("%y")+" at "+timeInput.strftime("%I")+":"+timeInput.strftime("%M")+" "+timeInput.strftime("%p")+" in " )
 
+class userData:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.
+
 class message:
-    def __init__(self, user, time:dt.datetime, sent:str):
+    def __init__(self, user: userData, time:dt.datetime, sent:str):
         """_summary_
 
         Args:
-            user (str): _description_
-            time (dt.datetime): The time the message is sent
+            user (userData): _description_
+            time (dt.datetime): _description_
             sent (str): _description_
         """
         self.user = user;
 
-        self.user = str (self.user)
-
         self.time = time
-
-        self.time = self.time
 
         self.sent = sent
 
-        self.sent = str (self.sent)
-
-
     
-    def readMessage(self):
+    def readMessage(self) -> str:
         readFinal = self.user + " > " + datetimeToString(self.time,True) + ": " + self.sent
         return readFinal
+
+
 
 uin = message("Isaac",dt.datetime.now(),"Test")
 print(dt.datetime.now().strftime("%z"))
